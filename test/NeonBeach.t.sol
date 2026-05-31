2b58d51de0260def98043d7e4ca990db05915f2c
Cyrilange <cyril.r.a.salamite@gmail.com>
2026-05-31 12:20:09 +0200

    nft online

--- test/NeonBeach.t.sol
+++ test/NeonBeach.t.sol
@@ -7,6 +7,7 @@ import {NeonBeach} from "../code/NeonBeach.sol";
 contract NeonBeachTest is Test {
     NeonBeach public neonBeach;
     address public owner = address(1);
+    address public user1 = address(2);
 
     function setUp() public {
         
@@ -19,4 +20,18 @@ contract NeonBeachTest is Test {
         assertEq(neonBeach.name(), "TokenizerArt");
         assertEq(neonBeach.symbol(), "NEON");
     }
+
+    function testMintSucces() public view {
+        string memory sampleUri = "ipfs://bafybeiardzgyf3ifrcixf7ywmb2jvqzymyxipzyd55xsts5ueta3vt72fy";
+        
+    
+        vm.prank(user1);
+        neonBeach.safe_mint(user1, 0, sampleUri);
+
+
+        assertEq(neonBeach.balanceOf(user1), 1);
+        assertEq(neonBeach.ownerOf(0), user1);
+        assertEq(neonBeach.tokenURI(0), sampleUri);
+    }
+
 }
